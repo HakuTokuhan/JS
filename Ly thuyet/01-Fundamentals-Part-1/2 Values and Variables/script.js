a4a15a0efef0fb81dd67682107fca91c73434bbc
NIMW <tridan243@gmail.com>
2023-07-14 01:55:24 +0700

    push code ly thuyet

---
+++ Ly thuyet/01-Fundamentals-Part-1/2 Values and Variables/script.js
@@ -0,0 +1,19 @@
+let js = "amazing";
+console.log(49 + 7 - 15 + 24);
+
+console.log("Dan");
+console.log(24);
+
+let firstName = "Dan";
+let first;
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
---
+++ Ly thuyet/01-Fundamentals-Part-1/3 Data Types/script.js
@@ -0,0 +1,19 @@
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 24);
+// console.log(typeof "Dan");
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2003;
+console.log(typeof year);
+
+console.log(typeof null);
