5d9e0a995324166e6280f00fafc9b49dc846d1c1
NIMW <tridan243@gmail.com>
2023-07-14 01:56:35 +0700

    push code bai tap

---
+++ Bai tap/01-Fundamentals-Part-1/1 Values and Variables/script.js
@@ -0,0 +1,11 @@
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Viet Nam";
+let continent = "Asisa";
+let population = 100000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ Bai tap/01-Fundamentals-Part-1/2 Data Types/script.js
@@ -0,0 +1,17 @@
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let country = "Viet Nam";
+let continent = "Asisa";
+let population = 100000000;
+let islsand = true;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(islsand);
+console.log(language);
