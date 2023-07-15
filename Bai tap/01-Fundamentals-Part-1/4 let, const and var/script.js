d9f9bde1786bb1130ac6a348c1230b35038e32a1
NIMW <tridan243@gmail.com>
2023-07-16 03:24:13 +0700

    push code lý thuyet bai tap

---
+++ Bai tap/01-Fundamentals-Part-1/4 let, const and var/script.js
@@ -0,0 +1,6 @@
+let language;
+language = Vietnamese;
+
+const birthDay = 24;
+
+language = false;
---
+++ Ly thuyet/01-Fundamentals-Part-1/4 let, const and var/script.js
@@ -0,0 +1,10 @@
+let age = 30;
+age = 31;
+
+const birthYear = 2003;
+// birthYear = 2004;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
