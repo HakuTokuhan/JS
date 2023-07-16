fe875a49318c758197afad69e589af35ef2fa9c1
NIMW <tridan243@gmail.com>
2023-07-17 02:54:00 +0700

    push code ly thuyet

---
+++ Ly thuyet/01-Fundamentals-Part-1/5 Basic Operators/script.js
@@ -0,0 +1,25 @@
+const now = 2023;
+const ageDan = now - 2003;
+const ageDuong = now - 2004;
+console.log(ageDan, ageDuong);
+
+console.log(ageDan * 2, ageDuong / 10, 2 ** 3);
+// 2 ** 3 = 2^3
+
+const firstName = "Dan";
+const lastName = "Nguyen Chi";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageDan > ageDuong);
+console.log(ageDuong >= 18);
+
+const isFullAge = ageDuong >= 18;
+
+console.log(now - 2003 > now - 2004);
