1e80d2e4b07eea54a1131bd9e0feb361bd619bfc
NIMW <tridan243@gmail.com>
2023-07-23 23:54:03 +0700

    push code challenge 1

---
+++ Ly thuyet/01-Fundamentals-Part-1/CHALLENGE #1/script.js
@@ -0,0 +1,12 @@
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIMark);
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIJohn);
+
+console.log(BMIMark > BMIJohn);
