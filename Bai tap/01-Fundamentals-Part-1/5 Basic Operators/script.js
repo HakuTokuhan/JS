20c04daa0d047f00f285519d1a0d28fea38ad022
NIMW <tridan243@gmail.com>
2023-07-23 23:49:41 +0700

    push code bai tap 5

---
+++ Bai tap/01-Fundamentals-Part-1/5 Basic Operators/script.js
@@ -0,0 +1,12 @@
+let containHalfThePopulation = 100 / 2;
+console.log(containHalfThePopulation);
+
+containHalfThePopulation += 1;
+console.log(containHalfThePopulation);
+
+console.log(containHalfThePopulation > 6);
+
+console.log(containHalfThePopulation < 33);
+
+let numberOfPortugueseSpeakersInEurope = 11;
+console.log(numberOfPortugueseSpeakersInEurope);
