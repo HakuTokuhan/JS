bdc58f9b291d30a8f4da73769e70774b418b24d2
NIMW <tridan243@gmail.com>
2023-07-23 23:53:19 +0700

    push code ly thuyet bai tap 8

---
+++ Bai tap/01-Fundamentals-Part-1/8 if else Statements/script.js
@@ -0,0 +1,13 @@
+let myCountryPopulation = 100;
+if (myCountryPopulation > 33) {
+  console.log("Portugal's population is above average");
+} else {
+  console.log("Portugal's population is 22 million below average");
+}
+
+myCountryPopulation = 13;
+if (myCountryPopulation > 33) {
+  console.log("Portugal's population is above average");
+} else {
+  console.log("Portugal's population is 22 million below average");
+}
---
+++ Ly thuyet/01-Fundamentals-Part-1/8 if else Statements/script.js
@@ -0,0 +1,17 @@
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license");
+} else {
+  const yearLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
+}
+
+const birthYear = 1998;
+
+if (birthYear <= 2000) {
+  let century = 20;
+} else {
+  let century = 21;
+}
+console.log(century);
