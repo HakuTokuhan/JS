b215a68ca9a5e4a9a483cf4f8fb40bd0ac5cfcc9
NIMW <tridan243@gmail.com>
2023-07-23 23:51:24 +0700

    push code ly thuyet 6

---
+++ Ly thuyet/01-Fundamentals-Part-1/6 operator precedence/script.js
@@ -0,0 +1,13 @@
+const now = 2037;
+const ageDan = now - 2003;
+const ageDuong = now - 2004;
+
+console.log(now - 2003 > now - 2004);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+const averageAge = (ageDan + ageDuong) / 2;
+console.log(ageDan, ageDuong, averageAge);
