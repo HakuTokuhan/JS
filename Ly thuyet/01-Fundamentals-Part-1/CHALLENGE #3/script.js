9974c9b65f71ee35d5996a497f94d6eb76ce152f
NIMW <tridan243@gmail.com>
2023-07-29 01:14:06 +0700

    push code ly thuyet

---
+++ Ly thuyet/01-Fundamentals-Part-1/CHALLENGE #3/script.js
@@ -0,0 +1,35 @@
+let scoreDolphins;
+let scoreKoalas;
+
+scoreDolphins = Number(prompt(console.log("Dolphins scored: ")));
+scoreKoalas = Number(prompt(console.log("Koalas scored:")));
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
+
+scoreDolphins = Number(prompt(console.log("Dolphins scored: ")));
+scoreKoalas = Number(prompt(console.log("Koalas scored:")));
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
+
+scoreDolphins = Number(prompt(console.log("Dolphins scored: ")));
+scoreKoalas = Number(prompt(console.log("Koalas scored:")));
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreDolphins < scoreKoalas) {
+  console.log("Koalas win the trophy");
+} else {
+  console.log("Both win the trophy");
+}
