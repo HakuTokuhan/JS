c02ed9523ea14d7d7bea151b516da624c62e3270
NIMW <tridan243@gmail.com>
2023-07-30 02:23:39 +0700

    push code bai tap

---
+++ Bai tap/01-Fundamentals-Part-1/15 The Conditional (Ternary) Operator/script.js
@@ -0,0 +1,20 @@
+let countryPopulation = 34;
+console.log(
+  `Portugal's population is ${
+    countryPopulation >= 33 ? "above" : "below"
+  } average`
+);
+
+countryPopulation = 13;
+console.log(
+  `Portugal's population is ${
+    countryPopulation >= 33 ? "above" : "below"
+  } average`
+);
+
+countryPopulation = 130;
+console.log(
+  `Portugal's population is ${
+    countryPopulation >= 33 ? "above" : "below"
+  } average`
+);
