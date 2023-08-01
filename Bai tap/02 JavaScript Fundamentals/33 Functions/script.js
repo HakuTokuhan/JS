e4cf4ee0ce2e5ec2928b62d89a0339910c12d0a5
NIMW <tridan243@gmail.com>
2023-08-02 02:34:40 +0700

    push code bai tap

---
+++ Bai tap/02 JavaScript Fundamentals/33 Functions/script.js
@@ -0,0 +1,8 @@
+function describeCountry(country, population, capitalCity) {
+  console.log(`${country} has ${population} million people and its 
+capital city is ${capitalCity}`);
+}
+
+describeCountry("Viet Nam", 100, "Ha Noi");
+describeCountry("Trung Quoc", 2300, "Bac Kinh");
+describeCountry("Phap", 20, "Paris");
