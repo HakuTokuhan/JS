1eab6045024df02ca8959b8d6aa9038fb6b3bb87
NIMW <tridan243@gmail.com>
2023-08-09 00:50:42 +0700

    push code bai tap

---
+++ Bai tap/02 JavaScript Fundamentals/35 Arrow Functions/script.js
@@ -0,0 +1,31 @@
+// function percentageOfWorld1(country, population) {
+//   console.log(
+//     `${country} has ${population} million people, so it's about ${
+//       (population / 7900) * 100
+//     }% of the world population`
+//   );
+// }
+
+// percentageOfWorld1("Trung Quoc", 1441);
+// percentageOfWorld1("Viet Nam", 100);
+// percentageOfWorld1("Han Quoc", 40);
+
+// let percentageOfWorld2 = function (country, population) {
+//   console.log(
+//     `${country} has ${population} million people, so it's about ${
+//       (population / 7900) * 100
+//     }% of the world population`
+//   );
+// };
+
+// percentageOfWorld2("Trung Quoc", 1441);
+// percentageOfWorld2("Viet Nam", 100);
+// percentageOfWorld2("Han Quoc", 40);
+
+const percentageOfWorld3 = (country, population) => {
+  return `${country} has ${population} million people, so it's about ${
+    (population / 7900) * 100
+  }% of the world population`;
+};
+
+console.log(percentageOfWorld3("Viet Nam", 100));
