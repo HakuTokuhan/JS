26f0f6b28b529c3f9a7c2547d92359e07bec66db
NIMW <tridan243@gmail.com>
2023-08-11 10:28:48 +0700

    push code ly thuyet

---
+++ Ly thuyet/02 JavaScript Fundamentals/42 Introduction to Objects/script.js
@@ -0,0 +1,15 @@
+const jonasArray = [
+  "Jonas",
+  "Schmedtman",
+  2037 - 1991,
+  "teacher",
+  ["Michael", " Peter", "Steven"],
+];
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtman",
+  age: 2037 - 1991,
+  job: "teacher",
+  friend: ["Michael", " Peter", "Steven"],
+};
