c0376936bd4d74aaadffa9d18b96ec351a8c6f23
NIMW <tridan243@gmail.com>
2023-08-22 01:28:55 +0700

    push code ly thuyet

--- Ly thuyet/07 JavaScript in browser DOM and event fundamentals/84 85 86/script.js
+++ Ly thuyet/07 JavaScript in browser DOM and event fundamentals/84 85 86/script.js
@@ -3,10 +3,11 @@
 // Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('.score--0');
+const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0EL = document.getElementById('.current--0');
-const current1EL = document.getElementById('.current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const bntNew = document.querySelector('.btn--new');
 const bntRoll = document.querySelector('.btn--roll');
@@ -23,14 +24,14 @@ const init = function () {
 
   score0El.textContent = 0;
   score1El.textContent = 0;
-  current0EL.textContent = 0;
-  current1EL.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
   diceEl.classList.add('hidden');
-  player0El.classList.remove('.player--winner');
-  player1El.classList.remove('.player--winner');
-  player0El.classList.add('.player--active');
-  player1El.classList.remove('.player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 };
 
 init();
@@ -43,19 +44,18 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// Rolling dice funtionality
+// Rolling dice functionality
 bntRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     // 2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1
-    if (dice != 1) {
+    if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
@@ -87,9 +87,10 @@ bntHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
     }
-    // Switch to the next player
-    switchPlayer();
   }
 });
 
---
+++ Ly thuyet/08 How JavaScript works behind the scenes/95 hoisting and TDZ in Practice/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// Variable
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+console.log(addEpxr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addEpxr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products delete!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
